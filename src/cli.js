89c3d67f3e0c51ee6d8ce20197a2f013af301443
polina karna <polina.karna@gmail.com>
2024-02-18 16:41:14 +0300

    added src/cli.js that greet the user

---
+++ src/cli.js
@@ -0,0 +1,11 @@
+import readlineSync from 'readline-sync';
+
+const greeting = () => {
+    console.log('Welcome to the Brain Games!')
+
+    const name = readlineSync.question('May I have your name? ');
+    console.log(`Hello, ${name}!`)
+    return;
+};
+
+export default greeting;
