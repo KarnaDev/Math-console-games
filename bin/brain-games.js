858f59d38e5d5e1077f4efc303f4e58151329ceb
polina karna <polina.karna@gmail.com>
2024-02-18 15:50:28 +0300

    change brain-games.js

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,2 +1,8 @@
-const greeting = () => console.log('Welcome to the Brain Games!');
-greeting()
+#!/usr/bin/env node
+// шебаг - сообщает операционной системе, что файл должен быть выполнен с использованием интерпретатора Node.js
+// env используется для поиска исполняемого файла node 
+
+console.log('Welcome to the Brain Games!');
+
+// const greeting = () => console.log('Welcome to the Brain Games!');
+// greeting()
